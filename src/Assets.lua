852ca652497b892db02654323133129a1c176a36
MynameisBI <20.21.9.12.1bibi@gmail.com>
2020-04-03 22:24:58 +0700

    Add SwordShield's walk, idle, block and Daggers' hit spritesheet

--- src/Assets.lua
+++ src/Assets.lua
@@ -3,7 +3,7 @@ local lg = love.graphics
 Sprites = {}
 
 Sprites.daggers = {}
-
 Sprites.daggers.idle  = lg.newImage('assets/characters/daggers/idle.png') -- 220 ms per frame
 Sprites.daggers.walk  = lg.newImage('assets/characters/daggers/walk.png') -- 140 ms per frame
 Sprites.daggers.block = lg.newImage('assets/characters/daggers/block.png') -- 1 frame
+Sprites.daggers.hit   = lg.newImage('assets/characters/daggers/hit.png') -- 1 frame
