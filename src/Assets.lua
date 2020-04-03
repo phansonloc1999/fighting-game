9c82ea41605e8f65a69d9fba88fe07254559deda
MynameisBI <20.21.9.12.1bibi@gmail.com>
2020-04-03 16:59:43 +0700

    Add block sprite

--- src/Assets.lua
+++ src/Assets.lua
@@ -4,5 +4,6 @@ Sprites = {}
 
 Sprites.daggers = {}
 
-Sprites.daggers.idle = lg.newImage('assets/characters/daggers/idle.png') -- 220 ms per frame
-Sprites.daggers.walk = lg.newImage('assets/characters/daggers/walk.png') -- 140 ms per frame
+Sprites.daggers.idle  = lg.newImage('assets/characters/daggers/idle.png') -- 220 ms per frame
+Sprites.daggers.walk  = lg.newImage('assets/characters/daggers/walk.png') -- 140 ms per frame
+Sprites.daggers.block = lg.newImage('assets/characters/daggers/block.png') -- 1 frame
